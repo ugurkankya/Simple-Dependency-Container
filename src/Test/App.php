66c3b807c358317527d8064b33d3c1385caf7a2f
Ugurkan Kaya <info@vevion.net>
2017-12-29 01:07:23 +0100

    Add files via upload

--- src/Test/App.php
+++ src/Test/App.php
@@ -1,1 +1,27 @@
+<?php
+/**
+ * @author : Ugurkan Kaya
+ * @date   : 29.12.2017
+ */
 
+namespace Container\Test;
+
+use Container\Test\Connection\ConnectionManager;
+
+class App
+{
+    /**
+     * @var $connectionManager
+     */
+    protected $connectionManager;
+
+    public function __construct(ConnectionManager $connectionManager)
+    {
+        $this->connectionManager = $connectionManager;
+    }
+
+    public function getConnectionManager(): ConnectionManager
+    {
+        return $this->connectionManager;
+    }
+}
